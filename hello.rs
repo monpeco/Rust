959bbffe4b5f28a80bba9ba04979df9a77fbb2eb
monpeco <monpeco@gmail.com>
2018-12-25 13:28:59 +0000

    Initial commit

---
+++ hello.rs
@@ -0,0 +1,20 @@
+use std::{i8, i16, i32, i64, u8, u16, u32, u64, isize, usize, f32, f64};
+
+use std::io::stdin;
+
+fn main() {
+    println!("Hello World2!");
+    
+    let num = 10;  // by default variables are inmutable
+    
+    let mut age: i32 = 40;  // mutable and type i32
+    
+    let is_it_true: bool = true;
+    
+    let let_x: char = 'x';
+    
+    println!("I am {} years old", age);
+    
+    let (f_name, l_name) = ("Derek", "Banas");
+    
+}
