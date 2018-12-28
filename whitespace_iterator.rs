eff9a702e289c916e73c8ef27c17086448607dc1
monpeco <monpeco@gmail.com>
2018-12-28 22:47:39 +0000

    Add interator with whitespace

---
+++ whitespace_iterator.rs
@@ -0,0 +1,22 @@
+use std::{i8, i16, i32, i64, u8, u16, u32, u64, isize, usize, f32, f64};
+
+use std::io::stdin;
+
+fn main() {
+  let rand_string = "I am a random string";
+  
+  let mut iter = rand_string.split_whitespace();
+  
+  let mut indiv_word = iter.next();
+  
+  loop{
+    match indiv_word {
+      Some(x) => println!("{}", x),
+      None => break,
+    }
+    indiv_word = iter.next();
+  }
+  
+}
+
+
