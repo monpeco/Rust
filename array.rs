d5d328765a22c02e5612a6eae8acdac859217313
monpeco <monpeco@gmail.com>
2018-12-29 16:29:04 +0000

    Add arrays

---
+++ array.rs
@@ -0,0 +1,17 @@
+use std::{i8, i16, i32, i64, u8, u16, u32, u64, isize, usize, f32, f64};
+
+use std::io::stdin;
+
+fn main() {
+  let rand_array = [1,2,3];
+
+  println!("{}", rand_array[0]);
+
+  println!("{}", rand_array.len());
+
+  println!("Second 2: {:?}", &rand_array[1..3]);
+
+  
+}
+
+
